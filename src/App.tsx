d801075a335e7eed0c6c3a1654166df2a1aaf2fa
Jeffrey Gao <jeffrey_gao@berkeley.edu>
2025-08-04 04:04:17 -0700

    about us done w vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ScrollToTop } from "@/components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Team from "./pages/Team";
 import ContactUs from "./pages/ContactUs";
@@ -28,6 +29,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
